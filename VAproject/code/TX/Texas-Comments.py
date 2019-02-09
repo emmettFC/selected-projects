782fd6b6392b6c1f6ca0685b56fd3989b6d7e9bf
emmettFC <emmettfculhane@gmail.com>
2019-02-09 14:34:10 -0500

    resolving memory issues and syncing dirs

--- VAproject/code/TX/Texas-Comments.py
+++
@@ -1,5 +0,0 @@
-'''
-    Texas: Registry does not require scraping, state permits full DB download
-        I: Stand up db and produce file for assimilation
-        II: Make process in bash for documentation
-'''
